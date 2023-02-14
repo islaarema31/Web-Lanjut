9c0108e3d5f2bffbc5fd3057879ad25865e12def
Agung Malik Al Qindy <agungalqindy@gmail.com>
2023-02-14 16:45:23 +0700

    test

--- Pertemuan1/resources/views/welcome.blade.php
+++ Pertemuan1/resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white"> Muhammad Islahuddin</h1></div>
+        <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white"> Muhammad Islahuddin S </h1></div>
         </div>
     </body>
 </html>
